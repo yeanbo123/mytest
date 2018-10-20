f76bfe9dbfbb7c99890595946b3ed1af6a743280
yeanbo <1255640857@qq.com>
2018-10-20 16:15:09 +0800

    添加git_test

---
+++ git_test_idea/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,16 @@
+package com.itheima.domain;
+
+/**
+ * Created with IDEA
+ * author:AnBoYe
+ * Date:2018/10/20
+ * Time:16:08
+ **/
+public class User {
+
+    private Integer id;
+
+    private String username;
+
+
+}
